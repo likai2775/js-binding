b564cf785d8ca4358196ea6f1bb5c79a3d6deaf3
sqrrrl <sqrrrl@568d4770-f052-0410-a86e-fd3cdf7867e6>
2008-07-28 03:17:08 +0000

    RefactorSplit property accessor from form bind preliminary type conversion for forms

--- trunk/src/js/binder.js
+++ trunk/src/js/binder.js
@@ -12,7 +12,9 @@
 //   See the License for the specific language governing permissions and
  //  limitations under the License.
 
-var Binder = function() {
+var Binder = {};
+
+Binder.PropertyAccessor = function() {
   var ARRAY_MATCH = /(.*)\[(\d*)\]/;
   var _setProperty = function( target, path, value ) {
     if( path.length == 0 ) {
@@ -68,16 +70,6 @@ var Binder = function() {
       }	
     }
   };
-  var _isSelected = function( value, options ) {
-    if( options instanceof Array ) {
-	  for( var i = 0; i < options.length; ++i ) {
-	    if( value == options[i] ) return true;	 
-	  }
-    } else {
-	  return value == options;
-    }
-    return false;
-  }
   return {
 	bindTo: function( obj ) {
       return {
@@ -96,47 +88,106 @@ var Binder = function() {
              return props;
           }
 	   };		
-	},
-	serializeForm: function( form, obj ) {
-      var binder = this.bindTo( obj );
-      for( var i = 0; i < form.elements.length; i++) {
-        var element = form.elements[i];
-        var value = undefined
-        if( element.type == "radio" || element.type == "checkbox" )  {
-          if( element.checked ) {
-            value = element.value;	
-          }
-        } else if ( element.type == "select-one" || element.type == "select-multiple" ) {
-          for( var j = 0; j < element.options.length; j++ ) {
-            if( element.options[j].selected ) {
-              binder.set( element.name, element.options[j].value );
-            }  
-          }
-        } else {
-	      value = element.value;
-      	}
-        if( value ) {
-          binder.set( element.name, value );	
-        }
-      }
-      return binder.target;
-	},
-	deserializeForm: function( form, obj ) {
-      var binder = this.bindTo( obj );
-      for( var i = 0; i < form.elements.length; i++) {
-        var element = form.elements[i];
-        var value = binder.get( element.name );
-        if( element.type == "radio" || element.type == "checkbox" )  {
-          element.checked = _isSelected( element.value, value );
-        } else if ( element.type == "select-one" || element.type == "select-multiple" ) {
-          for( var j = 0; j < element.options.length; j++ ) {
-	        element.options[j].selected = _isSelected( element.options[j].value, value );
-          }
-        } else {
-	      element.value = value ? value : "";
-      	}
-      }
-      return binder.target;
+	}
+  };
+}();
+
+
+Binder.FormBinder = function() {
+  var TYPE_MATCH = /type\[(.*)\]/;
+  var NUMBER_HANDLER = {
+    format: function( value ) {
+      return String( value );	
+    },
+    parse: function( value ) {
+      return Number( value );	
+    }
+  };
+  var _isSelected = function( value, options ) {
+    if( options instanceof Array ) {
+	  for( var i = 0; i < options.length; ++i ) {
+	    if( value == options[i] ) {
+		  return true;
+		}	 
+	  }
+    } else {
+	  return value == options;
+    }
+    return false;
+  };
+  var _getType = function( element ) {
+    if( element.className ) {
+ 	  var m = element.className.match( TYPE_MATCH );
+      if( m && m[1] ) {
+        return m[1];
+	  }
+	}
+	return "string";
+  };
+  var _format = function( path, value, element ) {
+    var type = _getType( element );
+    var handler = this.typeHandlers[type];
+    return handler ? handler.format( value ) : value;	
+  };
+  var _parse = function( path, value, element ) {
+    var type = _getType( element );
+    var handler = this.typeHandlers[type];
+    return handler ? handler.parse( value ) : value;	
+  };
+  return {
+	bind: function( form, handlers ) {
+      var self = this;
+	  this.typeHandlers = {
+	    number: NUMBER_HANDLER
+	  };
+	  for( prop in handlers ) {
+	    this.typeHandlers[prop] = handlers[prop];	 
+	  };
+	  return {
+		serialize: function( obj ) {
+	      var accessor = Binder.PropertyAccessor.bindTo( obj );
+	      for( var i = 0; i < form.elements.length; i++) {
+	        var element = form.elements[i];
+	        var value = undefined
+	        if( element.type == "radio" || element.type == "checkbox" )  {
+	          if( element.checked ) {
+	            value = _parse.apply( self, [ element.name, element.value, element ] );	
+	          }
+	        } else if ( element.type == "select-one" || element.type == "select-multiple" ) {
+	          for( var j = 0; j < element.options.length; j++ ) {
+	            if( element.options[j].selected ) {
+		          var v = _parse.apply( self, [ element.name, element.options[j].value, element ] );
+	              accessor.set( element.name, v );
+	            }  
+	          }
+	        } else {
+		      value = _parse.apply( self, [ element.name, element.value, element ] );
+	      	}
+	        if( value ) {
+	          accessor.set( element.name, value );	
+	        }
+	      }
+	      return accessor.target;
+		},
+		deserialize: function( obj ) {
+	      var accessor = Binder.PropertyAccessor.bindTo( obj );
+	      for( var i = 0; i < form.elements.length; i++) {
+	        var element = form.elements[i];
+	        var value = accessor.get( element.name );
+	        if( element.type == "radio" || element.type == "checkbox" )  {
+	          element.checked = _isSelected( element.value, value );
+	        } else if ( element.type == "select-one" || element.type == "select-multiple" ) {
+	          for( var j = 0; j < element.options.length; j++ ) {
+		        element.options[j].selected = _isSelected( element.options[j].value, value );
+	          }
+	        } else {
+		      value = _format.apply( self, [ element.name, value, element ] );
+		      element.value = value || "";
+	      	}
+	      }
+	      return accessor.target;
+		}
+	  };
 	}
   };
 }();
